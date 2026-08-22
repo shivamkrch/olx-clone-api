04410519bf37ec09b9e6dc6a1ed67a76550a2678
Shivam Kumar <shivamkmr.50397@gmail.com>
2026-08-22 22:54:41 +0530

    Add server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("Server is running....")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{ "status": "ok" }`))
+	})
+
+	srv := http.Server{
+		Addr:         ":8080",
+		Handler:      mux,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+		IdleTimeout:  time.Second * 60,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Server start failed: %v", err)
+	}
 }
