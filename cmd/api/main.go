5f2d67f11eb8b826e5fe1c0741767274052769a6
Shivam Kumar <shivamkmr.50397@gmail.com>
2026-08-23 08:10:07 +0530

    added config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,25 +4,30 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/shivamkrch/olx-clone-api/internal/config"
 )
 
 func main() {
+	cfg := config.MustLoad()
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{ "status": "ok" }`))
+		w.Write([]byte(`{"status": "ok"}`))
 	})
 
 	srv := http.Server{
-		Addr:         ":8080",
+		Addr:         ":" + cfg.Port,
 		Handler:      mux,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout:  time.Second * 60,
 	}
 
+	log.Printf("%s server is listening on %s", cfg.Env, srv.Addr)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("Server start failed: %v", err)
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shivamkrch/olx-clone-api
 
 go 1.26.6
+
+require github.com/joho/godotenv v1.5.1 // indirect
